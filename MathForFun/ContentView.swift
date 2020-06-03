06b6bc715494610dcbd832e593ff3f78b789b57d
sajan <sajanshrestharm11@gmail.com>
2020-06-03 10:44:27 -0400

    Initial Commit

---
+++ MathForFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathForFun
+//
+//  Created by Sajan Shrestha on 6/3/20.
+//  Copyright © 2020 Sajan Shrestha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
